052b5a3bcbd8eb25eab6bcf08db4e9e086977fc8
unknown <iamadamhussain@gmail.com>
2022-10-17 01:21:47 +0530

    second commit

--- src/main/java/com/fss/Application.java
+++ src/main/java/com/fss/Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application
 {
-
+//http://localhost:8081/swagger-ui.html#
 	public static void main(String[] args)
 	{
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/fss/controller/EmployeeServiceController.java
+++ src/main/java/com/fss/controller/EmployeeServiceController.java
@@ -14,18 +14,23 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fss.model.Employee;
 import com.fss.service.EmployeeService;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 @RestController
+@Api(value = "API to search Student from a Employee Repository by different search parameters",
+description = "This API provides the capability to search Employee from a Employee Repository", produces = "application/json")
 public class EmployeeServiceController {
 	@Autowired
 	private EmployeeService employeeService;
-
+	   @ApiOperation(value = "create Student", produces = "application/json")
 	@RequestMapping(value = "/employees", method = RequestMethod.POST)
 	public ResponseEntity<Object> createEmployee(@RequestBody Employee employee) {
 		employee = employeeService.createEmployee(employee);
 		return new ResponseEntity<>("Employee is created successfully with id = " + employee.getId(),
 				HttpStatus.CREATED);
 	}
-
+	   @ApiOperation(value = "updated employee by empId", produces = "application/json")
 	@RequestMapping(value = "/updateemployee/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Object> updateEmployee(@PathVariable("id") int id, @RequestBody Employee employee) {
 
@@ -34,7 +39,7 @@ public class EmployeeServiceController {
 		return new ResponseEntity<>("Employee is updated successsfully", HttpStatus.OK);
 
 	}
-
+	    @ApiOperation(value = "get for  employee by id", produces = "application/json")
 	@RequestMapping(value = "/getemployees/{id}", method = RequestMethod.GET)
 	public ResponseEntity<Object> getEmployee(@PathVariable("id") int id) {
 		boolean isEmployeeExist = employeeService.isEmployeeExist(id);
@@ -43,13 +48,13 @@ public class EmployeeServiceController {
 		return new ResponseEntity<>(employee, HttpStatus.OK);
 
 	}
-
+	    @ApiOperation(value = "get all employees", produces = "application/json")
 	@RequestMapping(value = "/listemployees", method = RequestMethod.GET)
 	public ResponseEntity<Object> getEmployees() {
 		List<Employee> employeeList = employeeService.getEmployees();
 		return new ResponseEntity<>(employeeList, HttpStatus.OK);
 	}
-
+	    @ApiOperation(value = "delete employee by id", produces = "application/json")
 	@RequestMapping(value = "/deleteemployee/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Object> deleteEmployee(@PathVariable("id") int id) {
 
